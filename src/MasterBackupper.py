0927c9a8773f3149333ac9e8e3cd97029d10a0ed
NOSER\oriol.gut <oriol.gut@noser.com>
2018-05-25 15:51:08 +0200

    corrected zipping and added title to folder dialog

--- src/MasterBackupper.py
+++ src/MasterBackupper.py
@@ -2,18 +2,12 @@ from tkinter.filedialog import askdirectory
 import os
 import zipfile
 
-source = askdirectory()
-print (source)
-destination = askdirectory()
-print (destination)
+source = askdirectory(title = 'Source Folder')
 
-def zipdir(path, ziph):
-    # ziph is zipfile handle
-    for root, files in os.walk(path):
-        for file in files:
-            ziph.write(os.path.join(root, file))
+destination = askdirectory(title = 'Destination Folder')
 
-
-    zipf = zipfile.ZipFile(destination+'/heutigesdatum.zip', 'w', zipfile.ZIP_DEFLATED)
-    zipdir(source, zipf)
-    zipf.close()
+zipf = zipfile.ZipFile(destination, 'w', zipfile.ZIP_DEFLATED)
+dirs = os.listdir(source)
+for file in dirs:
+    zipf.write(source + '/' + file)
+zipf.close()
