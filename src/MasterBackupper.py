628ca0cf29b9c045f464dca6f4c49fda6e8cd1fc
NOSER\oriol.gut <oriol.gut@noser.com>
2018-06-01 13:14:17 +0200

    completed script

--- src/MasterBackupper.py
+++ src/MasterBackupper.py
@@ -1,12 +1,15 @@
 from tkinter.filedialog import askdirectory
 import os
 import zipfile
+import datetime
+
+now = datetime.datetime.now()
 
 source = askdirectory(title = 'Source Folder')
 
 destination = askdirectory(title = 'Destination Folder')
 
-zipf = zipfile.ZipFile(destination, 'w', zipfile.ZIP_DEFLATED)
+zipf = zipfile.ZipFile(destination + '/backup' + now.strftime('%Y%m%d%H%M') + '.zip', 'w', zipfile.ZIP_DEFLATED)
 dirs = os.listdir(source)
 for file in dirs:
     zipf.write(source + '/' + file)
