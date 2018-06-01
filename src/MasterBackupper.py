a64aa4880a1699d04ca85d242e65f88c791a9fa9
NOSER\oriol.gut <oriol.gut@noser.com>
2018-06-01 13:30:02 +0200

    added check if script should be executed

--- src/MasterBackupper.py
+++ src/MasterBackupper.py
@@ -2,11 +2,16 @@ from tkinter.filedialog import askdirectory
 import os
 import zipfile
 import datetime
+import sys
+
+print('Should MasterBackupper start? (Y/N)')
+answer = input()
+
+if answer.lower() == 'n':
+    sys.exit(0)
 
 now = datetime.datetime.now()
-
 source = askdirectory(title = 'Source Folder')
-
 destination = askdirectory(title = 'Destination Folder')
 
 zipf = zipfile.ZipFile(destination + '/backup' + now.strftime('%Y%m%d%H%M') + '.zip', 'w', zipfile.ZIP_DEFLATED)
